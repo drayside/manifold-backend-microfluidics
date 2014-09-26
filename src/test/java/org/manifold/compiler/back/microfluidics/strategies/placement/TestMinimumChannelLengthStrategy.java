65c794a548e523eb46195d648dbef6909edd056e
Murphy Berzish <murphy.berzish@gmail.com>
2014-09-26 12:41:39 -0400

    test minimum channel length strategy

--- src/test/java/org/manifold/compiler/back/microfluidics/strategies/placement/TestMinimumChannelLengthStrategy.java
+++ src/test/java/org/manifold/compiler/back/microfluidics/strategies/placement/TestMinimumChannelLengthStrategy.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import java.util.List;
 
 import org.junit.Test;
+import org.manifold.compiler.ConnectionValue;
 import org.manifold.compiler.NodeValue;
 import org.manifold.compiler.back.microfluidics.MicrofluidicsBackend;
 import org.manifold.compiler.back.microfluidics.PrimitiveTypeTable;
@@ -16,30 +17,33 @@ import org.manifold.compiler.back.microfluidics.smt2.SymbolNameGenerator;
 import org.manifold.compiler.middle.Schematic;
 import org.manifold.compiler.middle.SchematicException;
 
-public class TestFiniteChipAreaRuleStrategy {
-
+public class TestMinimumChannelLengthStrategy {
   @Test
-  public void testViolation_OutOfBounds() throws SchematicException {
-    // create a single node and bind it outside the device area
+  public void testViolation_ZeroLength() throws SchematicException {
+ // create this schematic:
+    // (n1) --- (n2)
     Schematic sch = UtilSchematicConstruction.instantiateSchematic("test");
     NodeValue n1 = UtilSchematicConstruction.instantiatePressureControlPoint(
         sch, 1);
     sch.addNode("n1", n1);
+    NodeValue n2 = UtilSchematicConstruction.instantiatePressureControlPoint(
+        sch, 1);
+    sch.addNode("n2", n2);
+    ConnectionValue ch0 = UtilSchematicConstruction.instantiateChannel(
+        n1.getPort("channel0"), n2.getPort("channel0"));
+    sch.addConnection("ch0", ch0);
     
-    PrimitiveTypeTable typeTable = MicrofluidicsBackend.constructTypeTable(sch);
     ProcessParameters params = ProcessParameters.loadTestData();
+    PrimitiveTypeTable typeTable = MicrofluidicsBackend.constructTypeTable(sch);
     
-    ChipAreaRuleStrategy strat = new FiniteChipAreaRuleStrategy();
-    List<SExpression> exprs = strat.translate(sch, params, typeTable);
+    MinimumChannelLengthStrategy strat = new MinimumChannelLengthStrategy();
+    List<SExpression> exprs = strat.translationStep(sch, params, typeTable);
     
     AssertionChecker check = new AssertionChecker();
-    check.addBinding(SymbolNameGenerator.getsym_NodeX(
-        sch, n1), params.getMaximumChipSizeX() + 1.0);
-    check.addBinding(SymbolNameGenerator.getsym_NodeY(
-        sch, n1), -1.0);
+    check.addBinding(SymbolNameGenerator.getsym_ChannelLength(sch, ch0), 0.0);
+    
     if (check.verify(exprs)) {
       fail("failed to detect rule violation");
     }
   }
-  
 }
