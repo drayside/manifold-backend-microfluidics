7293b5f4e75a853fbb8be69c79e0fbd247bb934a
Divij Rajkumar <drajkuma1@gmail.com>
2015-03-18 10:40:29 -0400

    Modify channel length check to match java

--- src/main/matlab/+strategies/+placement/verifyMinimumChannelLengthStrategy.m
+++ src/main/matlab/+strategies/+placement/verifyMinimumChannelLengthStrategy.m
@@ -1,13 +1,3 @@
-function res = verifyMinimumChannelLengthStrategy(min_len, varargin)
-    import strategies.placement.verifyMinimumChannelLengthRule;
-
-    res = 1;
-    for idx = 1:numel(varargin)
-        channel = varargin{idx};
-        res = res & verifyMinimumChannelLength(min_len, channel);
-    end
-end
-
-function res = verifyMinimumChannelLength(min_len, channel)
-    res = channel.len >= min_len;
+function res = verifyMinimumChannelLength(chip, channel)
+    res = channel.len >= chip.minimumChannelLength;
 end
